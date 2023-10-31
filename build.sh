f3706883818e7683299537f253555a2dd5a26896
Tsai-TsungLin <ss19960429ss@gmail.com>
2023-10-31 14:01:11 +0800

    [fix] 修改docker 模式下也要有版本資訊

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ CommitHash=$(git rev-parse --short HEAD)
 CommitDate=$(git log -1 --format=%ai $(git describe --tags 2>/dev/null || echo HEAD))
 
 # Build the Docker image
-docker build -f Dockerfile -t newproject:1 . \
+docker build -f Dockerfile -t newProject:1 . \
     --build-arg TAG=$Tag \
     --build-arg COMMITHASH=$CommitHash \
     --build-arg COMMITDATE="$CommitDate"
